e77fc68c93150e853b605e66ad69c44dbf36fa64
ajwehunt <adamjwehunt@gmail.com>
2016-12-31 18:29:40 -0700

    codewars:shortestWord

--- javascript/beginnerLevel.js
+++ javascript/beginnerLevel.js
@@ -434,7 +434,16 @@ function sumTwoSmallestNumbers(nums) {
 ////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////
-
+// Simple, given a string of words, return the length of the shortest word(s).
+//
+// String will never be empty and you do not need to account for different data types.
+function findShort(s){
+  return s.split(' ').sort((a,b)=> a.length - b.length)[0].length
+}
+//top answer on codewars
+function findShort(s){
+  return Math.min.apply(null, s.split(' ').map(w => w.length));
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 
