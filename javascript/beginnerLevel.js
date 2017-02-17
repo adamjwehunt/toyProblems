a9c89cae1b33dba13c5b733ecb529f9425a87b93
ajwehunt <adamjwehunt@gmail.com>
2017-02-17 15:34:01 -0700

    more toyproblems and e6 case tut

--- javascript/beginnerLevel.js
+++ javascript/beginnerLevel.js
@@ -978,7 +978,36 @@ function reverse(str) {
 ////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////
+// You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns N.
+//
+// For example:
+//
+// [2, 4, 0, 100, 4, 11, 2602, 36]
+//
+// Should return: 11
+//
+// [160, 3, 1719, 19, 11, 13, -21]
+//
+// Should return: 160
 
+const findOutlier = (nums) => {
+  const odds = nums.filter(x=>x%2!==0),
+        evens = nums.filter(x=>x%2===0);
+  return odds.length > 1 ? evens[0] : odds[0]
+}
+
+//another good solution
+function findOutlier(integers){
+  var res;
+  var odd = integers.filter(function(e){
+    if(e%2 != 0){
+      res = e;
+    };
+    return e%2 == 0;
+  });
+
+  return odd.length>1 ? res : odd.pop();
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 
