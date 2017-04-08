6ec9aaac67b8a69c9941ad14180fb8fa28cb583d
ajwehunt <adamjwehunt@gmail.com>
2017-04-07 23:52:11 -0700

    lvl 6 added

--- javascript/beginnerLevel.js
+++ javascript/beginnerLevel.js
@@ -1571,9 +1571,42 @@ const capitals = word => { return word.match(/[A-Z]/g).map( x => { return word.i
 ////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////
+// Description:
+//
+// You are given an array strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+//
+// Example:
+//
+// longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+//
+// n being the length of the string array, if n = 0 or k > n or k <= 0 return "".
 
 
+function longestConsec(arr, k) {
+  let str = ''
+  if (k <= 0) {
+    return str
+  }
+  for (let i = 0; i < arr.length-k+1; i++) {
+    let x = arr.slice(i, i+k).join('')
+    if (str.length < x.length)
+      str = x
+  }
+  return str
+}
 
+//top
+function longestConsec(strarr, k) {
+    var longest = "";
+    for(var i=0;k>0 && i<=strarr.length-k;i++){
+      var tempArray = strarr.slice(i,i+k);
+      var tempStr = tempArray.join("");
+      if(tempStr.length > longest.length){
+        longest = tempStr;
+      }
+    }
+    return longest;
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 
