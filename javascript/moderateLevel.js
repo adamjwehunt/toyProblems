2ec7527c43609438d471d416d45ceb3ba27e4481
ajwehunt <adamjwehunt@gmail.com>
2017-04-18 23:07:46 -0700

    lvl 6

--- javascript/moderateLevel.js
+++ javascript/moderateLevel.js
@@ -625,6 +625,34 @@ function sum_pairs(nums, s){
 ////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////
+// Description:
+//
+// Your task is to construct a building which will be a pile of n cubes. The cube at the bottom will have a volume of n^3, the cube above will have volume of (n-1)^3 and so on until the top which will have a volume of 1^3.
+//
+// You are given the total volume m of the building. Being given m can you find the number n of cubes you will have to build?
+//
+// The parameter of the function findNb (find_nb, find-nb, findNb) will be an integer m and you have to return the integer n such as n^3 + (n-1)^3 + ... + 1^3 = m if such a n exists or -1 if there is no such n.
+
+function findNb(m) {
+  let total = 0
+  let i = 1
+  while (total < m) {
+    total = total + Math.pow(i, 3)
+    i++
+  }
+  if (total === m) {
+    return (i-1)
+  } else {
+    return -1
+  }
+}
+
+//top
+function findNb(m) {
+  var n = 0
+  while (m > 0) m -= ++n**3
+  return m ? -1 : n
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 
