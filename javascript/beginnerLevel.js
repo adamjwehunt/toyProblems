542310bc8fcc34bea8011b6fb471fc4cc5db596d
ajwehunt <adamjwehunt@gmail.com>
2017-07-31 20:07:15 -0700

    Another fizzbizz :D

--- javascript/beginnerLevel.js
+++ javascript/beginnerLevel.js
@@ -95,6 +95,19 @@ function fizzBuzz() {
   }
 }
 
+//best
+
+function fizzBuzz() {
+  for (var i = 1; i <= 100; i++) {
+    var output = "";
+    if (i % 3 === 0) { output += "fizz" }
+    if (i % 5 === 0) { output += "buzz" }
+
+    if (output === "") { output = i }
+    console.log(output);
+  }
+}
+
 //slimmed down
 function fizzBuzz() {
   for (var i = 1; i <= 100; i++) {
